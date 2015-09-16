fc2f5e65e5ea5df051678cb93d417a77460195bc
Jeff Wolinsky <jeff.wolinsky@makersquare.com>
2015-09-16 12:55:40 -0700

    initial

---
+++ indent.js
@@ -0,0 +1,12 @@
+var foo = function() {
+	console.log("i'm indented with a tab stop");
+    console.log("i'm indented with spaces");
+   	console.log("i'm indented with a mix");
+
+	var goo = function() {}
+		console.log("i'm indented with a tab stop");
+        console.log("i'm indented with spaces");
+ 		console.log("i'm indented with a mix");
+    };
+};
+    
